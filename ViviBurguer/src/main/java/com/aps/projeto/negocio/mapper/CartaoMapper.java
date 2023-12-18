95880ac7c04f34df65a79def2ee9be7721deb28e
Pedro Nogueira <pnc@cin.ufpe.br>
2023-12-18 09:00:24 -0300

    VVB-006 bufix on month

--- ViviBurguer/src/main/java/com/aps/projeto/negocio/mapper/CartaoMapper.java
+++ ViviBurguer/src/main/java/com/aps/projeto/negocio/mapper/CartaoMapper.java
@@ -19,14 +19,22 @@ public class CartaoMapper {
         .setTipoCartao(cartaoDTO.getTipoCartao())
         .setCpf(new CPF(cartaoDTO.getCpf()));
   }
+
   public static CartaoDTO cartaoToCartaoDto(Cartao cartao) {
     return new CartaoDTO()
         .setNumero(cartao.getNumero())
         .setCvv(cartao.getCvv())
         .setTitular(cartao.getTitular())
-        .setDataValidade(cartao.getDataValidade().getYear() + "-" + cartao.getDataValidade().getMonth().getValue())
+        .setDataValidade(cartao.getDataValidade().getYear() + "-" + getMonth(cartao.getDataValidade().getMonth().getValue()))
         .setBandeira(cartao.getBandeira())
         .setTipoCartao(cartao.getTipoCartao())
         .setCpf(cartao.getCpf().getCpf());
+  }
+  public static String getMonth(int month) {
+    String str = String.valueOf(month);
+    if(month < 10) {
+      str = "0" + str;
+    }
+    return str;
   }
 }
