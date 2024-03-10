7e908d734d7181ad98db32ca95a70441623ef948
zsmn <zsmn@cin.ufpe.br>
2024-03-10 20:05:09 -0300

    adding Dockerfile and compose for projects

--- viviburguer-microservices/account-register/src/main/java/br/com/viviburguer/accountregister/AccountRegisterApplication.java
+++ viviburguer-microservices/account-register/src/main/java/br/com/viviburguer/accountregister/AccountRegisterApplication.java
@@ -2,8 +2,14 @@ package br.com.viviburguer.accountregister;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("br.com.viviburguer.*")
+@ComponentScan(basePackages = { "br.com.viviburguer.*" })
+@EntityScan("br.com.viviburguer.*")
 public class AccountRegisterApplication {
 
 	public static void main(String[] args) {
